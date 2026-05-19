72dba000e4a21bee34386e7af7163b69d726261c
tharunbabu2003 <tharunbabu27@gmail.com>
2026-05-19 19:44:05 +0530

    Refactor AND gate implementation: enhance MSLoss and train functions for improved weight updates and loss tracking. Not working perfectly yet. Changes should be made

--- andGate.c
+++ andGate.c
@@ -1,14 +1,91 @@
 #include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+#include<string.h>
+#include<math.h>
 
+
+float MSLoss(int dataset[][3], int n, float weights[1][2], float b){
+    float cost = 0.0;
+    for(int i = 0; i < n; i++){
+        float y = 0.0;
+        for(int j = 0; j < 2; j++){
+            y += weights[0][j] * dataset[i][j];
+        }
+        y += b;
+        float sigmoid_y = 1/ (1 + exp(-1 * y));
+        float diff = sigmoid_y - dataset[i][2];
+        cost += diff * diff;
+    }
+    return cost/n;
+}
+
+void train(int dataset[][3], float lr, float step, int epochs, int n, float w[1][2], float *b){
+    //printf("in training...\n");
+    float db = 0.0f;
+    
+    for(int epoch= 1; epoch<= epochs; epoch++){
+        //printf("epoch count: %d\n", epoch);
+        float loss = MSLoss(dataset, n, w, *b); // loss should calculated in each epoch to calcalute finite derivative
+        float dw[1][2] = {0.0f, 0.0f};
+        
+        // dL/ dw0, dL/dw1
+        for(int i= 0; i< 2;i++){
+            float step_changed_weights[1][2];
+            memcpy(step_changed_weights, w, sizeof(step_changed_weights));
+            step_changed_weights[0][i] += step;
+            float step_changed_loss= MSLoss(dataset, n, step_changed_weights, *b);
+            dw[0][i]= (step_changed_loss - loss)/ step;
+        }
+        
+        // w0 = w0 - (lr * dL/ dw0), w1 = w1 - (lr * dL/ dw1)
+        for(int i=0; i<2; i++){
+            w[0][i] -= lr*dw[0][i];
+        }
+        
+        // b = b - (lr * dL/ db)
+        db = (MSLoss(dataset, n, w, *b + step) - loss)/ step;
+        *b -= lr*db;
+
+        printf("loss after %d epochs= %f\n", epoch, MSLoss(dataset, n, w, *b));
+    }
+}
+
+void testing(int dataset[][3], float weights[1][2], float b, int n){
+    for(int i=0; i< n; i++){
+        float y = weights[0][0] * dataset[i][0] + weights[0][1] * dataset[i][1] + b;
+        printf("i1= %d\t, i2= %d\t, y= %f\n", dataset[i][0], dataset[i][1], y);
+    }
+}
 
 int main(){
-    int train[][3] = {
+    int training[][3] = {
         {0, 0, 0},
         {0, 1, 0},
         {1, 0, 0},
         {1, 1, 1}
     };
 
+    int n = sizeof(training) / sizeof(training[0]);
+
+    srand((unsigned int) time(NULL));
+
+    float weights[1][2] = {(float)rand() / RAND_MAX, (float)rand() / RAND_MAX};
+    float b = (float) rand()/ RAND_MAX;
+    printf("w0= %f, w1=%f, b= %f\n", weights[0][0], weights[0][1], b);
+
+    float loss= MSLoss(training, n, weights, b);
+    printf("loss: %f\n", loss);
+
+    float step= 1e-2;
+    float lr= 1e-2;
+    int epochs= 100000;
+
+    train(training, lr, step, epochs, n, weights, &b);
     
+    printf("w0= %f, w1=%f, b= %f\n", weights[0][0], weights[0][1], b);
+
+    testing(training, weights, b, n);
+
     return 0;
 }
